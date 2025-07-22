15e0b744b20b941131b438c657475edb05ff59f5
Deggen <d.kellenschwiler@bsvassociation.org>
2025-07-21 22:45:13 -0500

    migrate score column

--- src/storage/knex/all-migrations.ts
+++ src/storage/knex/all-migrations.ts
@@ -5,6 +5,7 @@ import { up as addTransactionsTableUp, down as addTransactionsTableDown } from '
 import { up as addedIndexesUp, down as addedIndexesDown } from './migrations/2024-07-18-001-indexes.js'
 import { up as enlargeUp, down as enlargeDown } from './migrations/2025-05-28-001-enlarge.js'
 import { up as gaspPaginationSupportUp, down as gaspPaginationSupportDown } from './migrations/2025-06-25-001-gasp-pagination-support.js'
+import { up as fixScoreColumnTypeUp, down as fixScoreColumnTypeDown } from './migrations/2025-07-22-001-fix-score-column-type.js'
 
 
 
@@ -22,7 +23,8 @@ const allMigrations: Migration[] = [
   { up: addTransactionsTableUp, down: addTransactionsTableDown },
   { up: addedIndexesUp, down: addedIndexesDown },
   { up: enlargeUp, down: enlargeDown },
-  { up: gaspPaginationSupportUp, down: gaspPaginationSupportDown }
+  { up: gaspPaginationSupportUp, down: gaspPaginationSupportDown },
+  { up: fixScoreColumnTypeUp, down: fixScoreColumnTypeDown }
 ]
 
 export default allMigrations
